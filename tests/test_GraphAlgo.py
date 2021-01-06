5924342352719b39f44ddeb8e409d086b026c695
liadn7 <liadnagi@gmail.com>
2021-01-06 16:34:28 +0200

    plot fixxx

--- tests/test_GraphAlgo.py
+++ tests/test_GraphAlgo.py
@@ -65,11 +65,4 @@ class TestGraphAlgo(TestCase):
         if temp == None:
             self.fail(temp, " == None")
 
-    def test_connected_components(self):
-        g = doSmallGraph()
-        g.add_edge(2, 1, 4)
-        g.add_edge(1, 2, 4)
-
-        grp = GraphAlgo(g)
-        grp.plot_graph()
 
